3807d02945cb7ba7f242d6a6ab70a87470474039
hyeok230 <hyeok230@naver.com>
2021-12-27 21:46:43 +0900

    bubbleSort

---
+++ Sorting/bubbleSort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubbleSort(int list[], int size) {
+    int temp;
+
+    for (int i = size - 1; i >= 0; i--) {
+        for (int j = 0; j < i; j++){
+            if (list[j] > list[j + 1]) {
+                temp = list[j];
+                list[j] = list[j + 1];
+                list[j+ 1] = temp;     
+            }            
+        }
+    }
+}
+
+void printList(int list[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", list[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int list[] = {5, 8, 7, 1, 6, 2, 4, 3, 9, 10};
+
+    printf("==============Before Sort==============\n");
+    printList(list, 10);
+    printf("===============After Sort===============\n");
+    bubbleSort(list, 10);
+    printList(list, 10);
+}
